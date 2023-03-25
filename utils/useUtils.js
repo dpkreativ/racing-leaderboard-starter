2b81ce7ff5c164356720215406ba34dd4c7017ef
Divine Orji <dpkreativ@gmail.com>
2023-03-25 15:51:36 +0100

    feat: create updateState function

--- utils/useUtils.js
+++ utils/useUtils.js
@@ -7,3 +7,19 @@ export function sortData(data) {
     a.duration > b.duration ? 1 : a.duration < b.duration ? -1 : 0
   );
 }
+
+// function to update state with value
+export function updateState(state, newValue) {
+  // check if new value exists
+  const i = state.findIndex((x) => x.$id === newValue.$id);
+
+  // update state
+  if (i !== -1) {
+    state[i] = newValue;
+  } else {
+    state = { ...state, ...newValue };
+  }
+
+  // sort data
+  sortData(state);
+}
